d049dcc286d71a3dfb7ddbb2452e2e85c1d5d08c
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2023-12-23 11:40:52 +0530

    InsertATIndex implementated in DLL

--- LinkList/DoublyLinkList.cpp
+++ LinkList/DoublyLinkList.cpp
@@ -49,11 +49,44 @@ class DoublyLinkList {
             }
         }
 
+        void InsertAtIndex(int index, int data) {
+            if(head == nullptr && index > 0) {return;}
+            if(head == nullptr && index == 0) {
+                head = new Node(data);
+                return;
+            }
+
+            if(index == 0) {
+                Node* newNode = new Node(data);
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+                return;
+            }
+
+            int count = -1;
+            Node* temp = head;
+
+            while (temp != nullptr)
+            {
+                count++;
+                if(index == count) {
+                    Node* newNode = new Node(data);
+                    newNode->next = temp;
+                    newNode->prev = temp->prev;
+                    temp->prev->next = newNode;
+                    temp->prev = newNode;
+                    break;
+                }
+                temp = temp->next;
+            }
+        }
+
         void PrintReverse() {
             cout<< "Reverse Print :";
             Node* temp = head;
 
-            while (temp->next != nullptr)
+            while (temp != nullptr && temp->next != nullptr)
             {
                 temp = temp->next;
             }
@@ -72,8 +105,8 @@ class DoublyLinkList {
 
             while (temp != nullptr) 
             {
-            cout<< temp->data << " ";
-            temp = temp->next;
+                cout<< temp->data << " ";
+                temp = temp->next;
             }
             cout<<endl;
         }
@@ -84,11 +117,13 @@ int main() {
 
     DoublyLinkList list;
 
-    list.InsertAtTail(1);
-    list.InsertAtTail(2);
-    list.InsertAtTail(3);
+    //list.InsertAtTail(1);
+    //list.InsertAtTail(2);
+    //list.InsertAtTail(3);
 
-    list.InsertAtHead(10);
+    list.InsertAtIndex(0, 4);
+    list.InsertAtIndex(0, 3);
+    list.InsertAtIndex(1, 5);
 
     list.Print();
     list.PrintReverse();
