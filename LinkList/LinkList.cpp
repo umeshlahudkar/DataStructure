8118fc3f626a60baa9ecfa5d1940f16b3b743df1
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2023-12-24 14:31:44 +0530

    new problems on LL

--- LinkList/LinkList.cpp
+++ LinkList/LinkList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 
 
@@ -23,6 +24,10 @@ class LinkList{
         LinkList() : head(nullptr) {}
 
         Node*& GetHead() {return head;}
+
+        void SetHead(Node*& node) {
+            head = node;
+        }
        
         void ConstructFromArray(int arr[], int size) {
             head = nullptr;
@@ -226,7 +231,7 @@ class LinkList{
                 Node* temp = current;
                 current = current->next;
 
-                delete temp;
+                //delete temp;
             }
 
             head = nullptr;
@@ -234,9 +239,7 @@ class LinkList{
 };
 
 
-void RemoveNthNodeFromEnd(LinkList* list , int n) {
-
-    Node*& head = list->GetHead();
+void RemoveNthNodeFromEnd(Node*& head , int n) {
     if(head == nullptr) {return;}
  
     Node* slow = head;
@@ -275,18 +278,125 @@ void RemoveNthNodeFromEnd(LinkList* list , int n) {
     delete temp;
 }
 
+Node* GetMiddleNode(Node*& head) {
+
+    if(head == nullptr) {return nullptr;}
+    
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return slow;
+}
+
+void RemoveDuplicatesFromSorted(Node*& head) {
+    if(head == nullptr || head->next == nullptr) {return;}
+
+    Node* current = head->next;
+    Node* prev = head;
+
+    while (current != nullptr)
+    {
+        if(current->data == prev->data) {
+            Node* temp = current;
+            current = current->next;
+
+            prev->next = current;
+            temp->next = nullptr;
+            delete temp;
+        }
+        else {
+            prev = prev->next;
+            current = current->next;
+        }
+    }
+}
+
+void RemoveDuplicatesFromUnsorted(Node*& head) {
+
+    if(head == nullptr || head->next == nullptr) {return;}
+
+    unordered_set<int> seen;
+    seen.insert(head->data);
+
+    Node* current = head->next;
+    Node* prev = head;
+
+    while (current != nullptr) {
+        if (seen.find(current->data) != seen.end()) {
+            Node* temp = current;
+            prev->next = current->next;
+            current = current->next;
+            delete temp;
+        } else {
+            seen.insert(current->data);
+            prev = prev->next;
+            current = current->next;
+        }
+    }
+}
+
+Node* MergeTwoList(Node* head1, Node* head2) {
+
+    Node* head = new Node(0);
+    Node* current = head;
+
+    while (head1 != nullptr && head2 != nullptr)
+    {
+        if(head1->data <= head2->data) {
+            current->next = head1;
+            head1 = head1->next;
+
+            current = current->next;
+        }
+        else {
+            current->next = head2;
+            head2 = head2->next;
+
+            current = current->next;
+        }
+    }
+
+    if(head1 == nullptr) {
+        current->next = head2;
+    }
+
+    if(head2 == nullptr) {
+        current->next = head1;
+    }
+
+    return head->next;
+}
+
 
 int main() {
 
-    int arr[] = {1,2,3,4,5};
-    int size = sizeof(arr)/sizeof(arr[0]);
+    int arr1[] = {1,3,5,7};
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
 
-    LinkList list;
-   
-    list.ConstructFromArray(arr, size);
+    LinkList list1;
+    list1.ConstructFromArray(arr1, size1);
 
-    RemoveNthNodeFromEnd(&list, 5);
+    list1.Print();
+
+    int arr2[] = {2,4};
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    LinkList list2;
+    list2.ConstructFromArray(arr2, size2);
+
+    list2.Print();
+    
+    Node* node = MergeTwoList(list1.GetHead(), list2.GetHead());
+
+    LinkList lint3;
+    lint3.SetHead(node);
+
+    lint3.Print();
      
-    list.Print();
     return 0;
 }
