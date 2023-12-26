7ff369474b28709551b47df84da18405d24418b4
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2023-12-26 21:56:05 +0530

    Update BinarySearch.cpp

--- Algorithm/BinarySearch.cpp
+++ Algorithm/BinarySearch.cpp
@@ -36,13 +36,95 @@ int BinarySearchByRecursive(int arr[], int low, int high, int number) {
     }
 }
 
+int LowerBound(int arr[], int size, int number) {
+    int low = 0, high = size-1, index = size;
 
+    int mid = 0;
+
+    while (low <= high)
+    {
+        mid = low + (high - low)/2;
+
+        if(arr[mid] >= number) {
+            index = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return index;
+}
+
+int UpperBound(int arr[], int size, int number) {
+    int low = 0, high = size-1, index = size;
+
+    int mid = 0;
+
+    while (low <= high)
+    {
+        mid = low + (high - low)/2;
+
+        if(arr[mid] > number) {
+            index = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return index;
+}
+
+int FloorValue(int arr[], int size, int number) {
+    int low = 0;
+    int high = size - 1;
+
+    int ans = -1;
+    int mid = 0;
+
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        if(arr[mid] <= number) {
+            ans = arr[mid];
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int CeilValue(int arr[], int size, int number) {
+    int low = 0;
+    int high = size - 1;
+
+    int ans = -1;
+    int mid = 0;
+
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        if(arr[mid] >= number) {
+            ans = arr[mid];
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 
 int main() {
-    int arr[] = {1,2,3,4,5,6};
+    int arr[] = {1,2,3,4,6};
     int size = sizeof(arr)/sizeof(arr[0]);
 
-    cout<< "Index : "<< BinarySearchByRecursive(arr, 0, size-1,  1) <<endl;
+    cout<< "Index : "<< CeilValue(arr, size,  5) <<endl;
 
 
    return 0;
