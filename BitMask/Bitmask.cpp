c22f451f3a78bc5b90e890f99b4827aa8ba43e5d
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2023-12-26 20:36:18 +0530

    bitmask

--- BitMask/Bitmask.cpp
+++ BitMask/Bitmask.cpp
@@ -25,13 +25,19 @@ int SetBit(int number, int bit) {
     return mask | number;
 }
 
+int ClearBit(int number, int bit) {
+    int mask = ~(1<< bit);
+
+    return (number & mask);
+}
+
 int main() {
     int arr[] = {1,1,2,2,3,4,4};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     //PrintSingleNumber(arr, size);
     
-   cout << "Set number : " << SetBit(13, 2);
+   cout << "Set number : " << ClearBit(13, 3);
 
 
     return 0;
